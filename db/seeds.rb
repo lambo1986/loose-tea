d0d0ca27c6704454d5ee39d233edcaaa76e950d2
Nathan Lambertson <lambertones@gmail.com>
2024-03-09 09:57:26 -0600

    Add teas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+43.times do
+  Tea.create!(
+    title: Faker::Tea.variety,
+    description: Faker::Fantasy::Tolkien.poem,
+    temperature: Faker::Number.between(from: 165, to: 212),
+    brew_time: Faker::Number.between(from: 2, to: 7)
+  )
+end
