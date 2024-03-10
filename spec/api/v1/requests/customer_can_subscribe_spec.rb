460ee37239a095d805724b2216f72cc1b6fd03e8
Nathan Lambertson <lambertones@gmail.com>
2024-03-10 08:50:02 -0500

    Enchance tests

--- spec/api/v1/requests/customer_can_subscribe_spec.rb
+++ spec/api/v1/requests/customer_can_subscribe_spec.rb
@@ -1,34 +1,41 @@
 require "rails_helper"
 
 RSpec.describe "customer subscriptions", type: :request do
+  let(:customer) { FactoryBot.create(:customer) }
+  let(:tea1) { FactoryBot.create(:tea) }
+  let(:tea2) { FactoryBot.create(:tea) }
+  let(:subscription_params) do
+    {
+      subscription: {
+        title: "Monthly Double Delight",
+        price: "19.99",
+        status: "active",
+        frequency: "monthly",
+        tea_ids: [tea1.id, tea2.id]
+      }
+    }
+  end
+
+  before do
+    post "/api/v1/customers/#{customer.id}/subscriptions", params: subscription_params
+  end
+
   describe "POST /api/v1/customers/subscriptions" do
     it "activates/adds a customer subscription" do
-      customer = FactoryBot.create(:customer)
-      tea1 = FactoryBot.create(:tea)
-      tea2 = FactoryBot.create(:tea)
-      subscription_params = {
-        subscription: {
-          title: "Monthly Double Delight",
-          price: "19.99",
-          status: "active",
-          frequency: "monthly",
-          tea_ids: [tea1.id, tea2.id]
-        }
-      }
-
-      post "/api/v1/customers/#{customer.id}/subscriptions", params: subscription_params
-
       expect(response).to have_http_status(:created)
 
       json_response = JSON.parse(response.body)
 
       expect(json_response["data"]["attributes"]["title"]).to eq("Monthly Double Delight")
       expect(json_response["data"]["attributes"]["status"]).to eq("active")
+      expect(json_response["included"].first["type"]).to eq("tea")
       expect(json_response["included"].count).to eq(2)
     end
   end
 
   describe "DELETE /api/v1/customers/:id/subscriptions/:subscription_id" do
+    it "deactivates a customer subscription" do
 
+    end
   end
 end
