46fd0b2df5e8d679734800a92e1b87132d0cc88b
Nathan Lambertson <lambertones@gmail.com>
2024-03-18 07:58:29 -0500

    Rename specs and add controller logic for customers

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,3 +1,11 @@
 class Api::V1::CustomersController < ApplicationController
-  
+  def create
+    require 'pry'; binding.pry
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :email, :address)
+  end
 end
